dbf8a94b37a7965fd41b340a959c6307610dbc40
arpitjain06 <arpitjain06@hotmail.com>
2016-05-20 20:06:17 +0530

    sync all the account from qb [Development phase]

---
+++ erpnext_quickbooks/sync_account.py
@@ -0,0 +1,65 @@
+from __future__ import unicode_literals
+import frappe
+from frappe import _
+import frappe.defaults
+import requests.exceptions
+from .utils import make_quickbooks_log
+
+"""Code to fetch all the Account from Quickbooks And store it in ERPNEXT"""
+def sync_Account(quickbooks_obj):
+	"""Fetch Account data from QuickBooks"""
+	quickbooks_account_list = []
+	account_query = """SELECT Name, Active, Classification, AccountType, AccountSubType, CurrencyRef, Id FROM Account order Id Desc""" 
+	qb_account = quickbooks_obj.query(account_query)
+	get_qb_account =  qb_account['QueryResponse']['Account']
+	sync_qb_accounts(get_qb_account,quickbooks_account_list)
+	
+def sync_qb_accounts(get_qb_account, quickbooks_account_list):
+	for qb_account in get_qb_account:
+		if not frappe.db.get_value("Account", {"quickbooks_account_id": qb_account.get('id')}, "name"):
+			create_account(qb_account, quickbooks_account_list)
+
+# def sync_qb_accounts(data):
+# 	for qb_account in data:
+# 		if not frappe.db.get_value("Account", {"quickbooks_account_id": qb_account.get('Id')}, "name"):
+# 			create_account(qb_account, quickbooks_account_list =[])
+
+def create_account(qb_account, quickbooks_account_list):
+	""" store Account data in ERPNEXT """ 
+	account = None
+	root_type = None
+	parent_account = None
+	Default_company = frappe.defaults.get_defaults().get("company")
+	
+	if qb_account.get('Classification') == "Asset":
+		parent_account = frappe.db.get_value("Account",{"company":Default_company,"name":"Debtors - ES"},"parent_account")
+		root_type = "Asset"
+	elif qb_account.get('Classification') == "Liability":
+		parent_account = frappe.db.get_value("Account",{"company":Default_company,"name":"Accounts Payable - ES"},"parent_account")
+		root_type = "Liability"
+	else:
+		parent_account = frappe.db.get_value("Account",{"company":Default_company,"name":"Indirect Expenses - ES"},"parent_account") 
+		root_type = "Expense"
+		
+	try:	
+		account = frappe.new_doc("Account")
+		account.quickbooks_account_id = str(qb_account.get('Id'))
+		account.account_name = str(qb_account.get('Name'))
+		account.is_group = False
+		account.parent_account = parent_account
+		account.root_type = root_type
+		account.account_currency = qb_account.get('CurrencyRef').get('value')
+		account.flags.ignore_mandatory = True
+		account.insert()
+
+		frappe.db.commit()
+		quickbooks_account_list.append(account.quickbooks_account_id)
+
+	except Exception, e:
+		if e.args[0] and e.args[0].startswith("402"):
+			raise e
+		else:
+			make_quickbooks_log(title=e.message, status="Error", method="create_account", message=frappe.get_traceback(),
+				request_data=qb_account, exception=True)
+	
+	return quickbooks_account_list
